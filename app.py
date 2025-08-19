212bfe3ba4f1ee0e5d121353345ecc2521939f5f
Lolamaria94 <lolacevallos30@gmail.com>
2025-08-19 17:05:17 -0500

    Mi primer comit

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+df= pd.read_csv("C:\\Users\\HP\\Documents\\Python\\Proyecto\vehicles_us.csv")
+
+st.header('Tipos de vehículos por manofactura')
+
+hist_button = st.button('Construir histograma') # crear un botón
+     
+if hist_button: # al hacer clic en el botón 
+         # escribir un mensaje
+         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+         fig = px.histogram(df, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+         st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
